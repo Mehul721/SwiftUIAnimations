5fd13a9a7eb4c9e12383cbd16f2869c250476857
Mehul721 <jmehul721@gmail.com>
2024-09-08 23:08:08 +0530

    Initial Commit

---
+++ SwiftUIAnimations/SwiftUIAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationsApp.swift
+//  SwiftUIAnimations
+//
+//  Created by Mehul Jain on 08/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
