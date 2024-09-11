68f51aa6774658531e174c18d43239b62e1cc1b3
Mehul721 <jmehul721@gmail.com>
2024-09-11 23:45:50 +0530

    Used custom fonts and changed text layout

--- SwiftUIAnimations/OnboardingView.swift
+++ SwiftUIAnimations/OnboardingView.swift
@@ -10,38 +10,47 @@ import RiveRuntime
 
 struct OnboardingView: View {
     let button = RiveViewModel(fileName: "button")
+    
     var body: some View {
         ZStack {
             background
-        }
-        button.view()
-            .frame(width:236,height:64)
-            .overlay(
-                Label("Start the course",systemImage: "arrow.forward")
-                    .offset(x:4,y:4)
-                    .font(.headline))
-            .background(
-                Color.black
-                    .clipShape(RoundedRectangle(cornerRadius: 30))
-                    .blur(radius:10)
-                    .opacity(0.3)
-                    .offset(y:10)
+            VStack {
+                Text("Learn design & code")
+                    .font(.custom("Poppins Bold", size: 60, relativeTo: .largeTitle))
+                    
                 
-            )
-            .onTapGesture {
-            try? button.play(animationName: "active"
-            )}
+                button.view()
+                    .frame(width: 236, height: 64)
+                    .overlay(
+                        Label("Start the course", systemImage: "arrow.forward")
+                            .offset(x: 4, y: 4)
+                            .font(.headline)
+                    )
+                    .background(
+                        Color.black
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                            .blur(radius: 10)
+                            .opacity(0.3)
+                            .offset(y: 10)
+                    )
+                    .onTapGesture {
+                        try? button.play(animationName: "active")
+                    }
+            }
+            
+        }
     }
-var background:some View{
-    RiveViewModel(fileName: "shapes").view()
-        .ignoresSafeArea()
-        .blur(radius:30)
-        .background(
-            Image("Spline")
-                .blur(radius:50)
-                .offset(x:200,y:100)
-    )
-}
+    
+    var background: some View {
+        RiveViewModel(fileName: "shapes").view()
+            .ignoresSafeArea()
+            .blur(radius: 30)
+            .background(
+                Image("Spline")
+                    .blur(radius: 50)
+                    .offset(x: 200, y: 100)
+            )
+    }
 }
 #Preview {
     OnboardingView()
