6b8406a50780967e774f29e315d068192ebd43f3
Mehul721 <jmehul721@gmail.com>
2024-09-17 23:55:25 +0530

    Completed custom login page

--- SwiftUIAnimations/SignInView.swift
+++ SwiftUIAnimations/SignInView.swift
@@ -8,13 +8,40 @@
 import SwiftUI
 
 struct SignInView: View {
+    @State var email=""
+    @State var password=""
     var body: some View {
         VStack(spacing:24) {
             Text("Sign In")
                 .customFont(.largeTitle)
             Text("Access to 240+ hrs of content.")
                 .customFont(.headline)
-            Divider()
+            
+            
+            VStack(alignment: .leading) {
+                Text("Email")
+                    .customFont(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                TextField("", text: $email)
+                    .customTextField()
+                    }
+            VStack(alignment: .leading) {
+                Text("Password")
+                    .customFont(.subheadline)
+                    .foregroundColor(.secondary)
+                
+                SecureField("", text: $password)
+                .customTextField(image:Image("Icon Lock"))}
+        
+            HStack {
+                Rectangle().frame(height:1).opacity(0.1)
+                
+                Text("OR").customFont(.subheadline2)
+                    .foregroundStyle(Color.black.opacity(0.3))
+                Rectangle().frame(height:1).opacity(0.1)
+            }
+            
             Text("Sign-up with E-mail,Apple or Google")
                 .customFont(.subheadline)
             HStack{
@@ -31,6 +58,8 @@ struct SignInView: View {
         .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))//the region around signin area highlighted.
         .shadow(color:Color("Shadow").opacity(0.3),radius: 5,x:0,y:4)
         .shadow(color:Color("Shadow").opacity(0.3),radius: 30,x:0,y:30)
+        .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
+            .stroke(.linearGradient(colors:[.white.opacity(0.8),.white.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing)))
         .padding()
     }
 }
