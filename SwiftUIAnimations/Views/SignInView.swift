0baad63c808d8f38a31aed5c884572952961a90f
Mehul721 <jmehul721@gmail.com>
2024-09-25 02:18:34 +0530

    tabbar animations done on icons withh rive

--- SwiftUIAnimations/Views/SignInView.swift
+++ SwiftUIAnimations/Views/SignInView.swift
@@ -100,7 +100,7 @@ struct SignInView: View {
                     .padding(40)
                     .background(.regularMaterial)
                     .mask(RoundedRectangle(cornerRadius: 20, style: .continuous))
-                    .shadow(color:Color("Shadow").opacity(0.2),radius: 5,x:0,y:4)
+                    .shadow(color:Color("Shadow").opacity(0.3),radius: 5,x:0,y:4)
                     .shadow(color:Color("Shadow").opacity(0.3),radius: 30,x:0,y:30)
                     .overlay(RoundedRectangle(cornerRadius: 20, style: .continuous)
                         .stroke(.linearGradient(colors:[.white.opacity(0.8),.white.opacity(0.1)], startPoint: .topLeading, endPoint: .bottomTrailing)))
