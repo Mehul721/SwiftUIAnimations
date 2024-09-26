dfe0f22134660231a9eaf8647246d8b6cdbed8e7
Mehul721 <jmehul721@gmail.com>
2024-09-26 23:58:36 +0530

    changes made to UI

--- SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
     var body: some View {
-        OnboardingView()
+        ZStack{
+            switch selectedTab {
+            case .chat:
+                Text("Chat")
+            case .search:
+                Text("Search")
+            case .timer:
+                Text("Timer")
+            case .bell:
+                Text("Bell")
+            case .user:
+                Text("User")
+            }
+            TabBar()
+        }
+        
     }
 }
 
 #Preview {
-    OnboardingView()
+    ContentView()
 }
