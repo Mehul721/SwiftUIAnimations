1bb7c4399ca1082bae3ac48e45777af3ca31e5b0
Mehul721 <jmehul721@gmail.com>
2024-09-27 23:56:23 +0530

    Burger icon animation

--- SwiftUIAnimations/ContentView.swift
+++ SwiftUIAnimations/ContentView.swift
@@ -4,11 +4,13 @@
 //
 //  Created by Mehul Jain on 08/09/24.
 //
-
 import SwiftUI
+import RiveRuntime
 
 struct ContentView: View {
     @AppStorage("selectedTab") var selectedTab: Tab = .chat
+    @State var isOpen = false
+    let button=RiveViewModel(fileName: "menu_button",animationName: "open",autoPlay: false)
     var body: some View {
         ZStack{
             switch selectedTab {
@@ -23,6 +25,16 @@ struct ContentView: View {
             case .user:
                 Text("User")
             }
+            button.view()
+                .frame(width: 44, height: 44)
+                .mask(Circle())
+                .shadow(color: .black, radius: 5, x: 0, y: 5)
+                .frame( maxWidth: .infinity, maxHeight: .infinity,alignment: .topLeading)
+                .padding(12)
+                .onTapGesture {
+                    try? button.setInput("isOpen", value: isOpen)
+                    isOpen.toggle()
+                }
             TabBar()
         }
         
