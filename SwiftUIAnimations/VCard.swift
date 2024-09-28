f9550dcfbca687cea890bb62233f18a2b2785eb2
Mehul721 <jmehul721@gmail.com>
2024-09-28 23:55:08 +0530

    Added the vcard along with design

---
+++ SwiftUIAnimations/VCard.swift
@@ -0,0 +1,18 @@
+//
+//  VCard.swift
+//  SwiftUIAnimations
+//
+//  Created by Mehul Jain on 28/09/24.
+//
+
+import SwiftUI
+
+struct VCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VCard()
+}
