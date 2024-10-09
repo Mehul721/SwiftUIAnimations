f1562eae900cba64f0a14bf79231de4afe2fdaa2
Mehul721 <jmehul721@gmail.com>
2024-10-09 22:22:41 +0530

    Animated the options in the sidetab

---
+++ SwiftUIAnimations/MenuRow.swift
@@ -0,0 +1,18 @@
+//
+//  MenuRow.swift
+//  SwiftUIAnimations
+//
+//  Created by Mehul Jain on 09/10/24.
+//
+
+import SwiftUI
+
+struct MenuRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuRow()
+}
