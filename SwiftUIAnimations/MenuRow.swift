746f63eaf4af555d92c1c79887b53f1d8ac91092
Mehul721 <jmehul721@gmail.com>
2024-10-09 22:23:39 +0530

    Animated the options in sidetab

--- SwiftUIAnimations/MenuRow.swift
+++ SwiftUIAnimations/MenuRow.swift
@@ -8,11 +8,39 @@
 import SwiftUI
 
 struct MenuRow: View {
+    var item: MenuItems
+    @Binding var selectedMenu: SelectedMenu
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing:14){
+            item.icon.view()
+                .frame(width:32,height:32)
+                .opacity(0.6)
+                .foregroundStyle(Color.white)
+            Text(item.text)
+                .customFont(.subheadline2)
+        }
+        .frame(maxWidth:.infinity,alignment:.leading)
+        .padding(8)
+        .background(
+            RoundedRectangle(cornerRadius:10,style: .continuous)
+                .fill(.blue)
+                .frame(maxWidth:selectedMenu == item.menu ? .infinity : 0)
+                .frame(maxWidth:.infinity,alignment:.leading)
+        )
+        .background(Color(hex: "17203A"))
+        .onTapGesture {
+            try? item.icon.setInput("active", value: true)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                try? item.icon.setInput("active", value: false)
+            }
+            withAnimation{
+                selectedMenu = item.menu
+            }
+        }
     }
 }
 
 #Preview {
-    MenuRow()
+    MenuRow(item:menuItems[0],selectedMenu: .constant(.home))
 }
--- SwiftUIAnimations/SideMenu.swift
+++ SwiftUIAnimations/SideMenu.swift
@@ -9,6 +9,10 @@ import SwiftUI
 import RiveRuntime
 
 struct SideMenu: View {
+    
+    @State var selectedMenu:SelectedMenu = .home
+    @State var isDarkMode:Bool = false
+    
     let icon=RiveViewModel(fileName: "icons", stateMachineName: "HOME_interactivity",artboardName: "HOME")
     var body: some View {
         
@@ -46,25 +50,34 @@ struct SideMenu: View {
                         .frame(height:1)
                         .opacity(0.1)
                         .padding(.horizontal)
-                    HStack(spacing:14){
-                        item.icon.view()
-                            .frame(width:32,height:32)
-                            .opacity(0.6)
-                            .foregroundStyle(Color.white)
-                        Text(item.text)
-                            .customFont(.subheadline2)
-                    }
-                    .frame(maxWidth:.infinity,alignment:.leading)
-                    .padding(8)
-                    .onTapGesture {
-                        try? item.icon.setInput("active", value: true)
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                            try? item.icon.setInput("active", value: false)
-                        }
-                    }
+                    MenuRow(item: item, selectedMenu: $selectedMenu)
                 }
         }
             .padding(8)
+            Text("HISTORY")
+                .customFont(.subheadline2)
+                .frame(maxWidth:.infinity,alignment: .leading)
+                .padding(.horizontal,24)
+                .padding(.top,40)
+                .opacity(0.7)
+            
+            
+            VStack(alignment: .leading, spacing: 0){
+                ForEach(menuItems2) { item in
+                    Rectangle()
+                        .frame(height:1)
+                        .opacity(0.1)
+                        .padding(.horizontal)
+                    MenuRow(item: item, selectedMenu: $selectedMenu)
+                }
+        }
+            .padding(8)
+            
+            HStack {
+               
+                Toggle("",isOn: $isDarkMode)
+                    .padding(.trailing,30)
+            }
             Spacer()
         }
         .foregroundStyle(Color.white)
@@ -82,10 +95,27 @@ struct MenuItems:Identifiable {
     var id:UUID = UUID()
     var text:String
     var icon:RiveViewModel
+    var menu:SelectedMenu
 }
 var menuItems:[MenuItems] = [
-    MenuItems(text:"Home",icon:RiveViewModel(fileName:"icons",artboardName:"HOME")),
-    MenuItems(text:"Search",icon:RiveViewModel(fileName:"icons",artboardName:"SEARCH")),
-    MenuItems(text:"Favorites",icon:RiveViewModel(fileName:"icons",artboardName:"LIKE/STAR")),
-    MenuItems(text:"Help",icon:RiveViewModel(fileName:"icons",artboardName:"CHAT"))
+    MenuItems(text:"Home",icon:RiveViewModel(fileName:"icons",artboardName:"HOME"),menu:.home),
+    MenuItems(text:"Search",icon:RiveViewModel(fileName:"icons",artboardName:"SEARCH"),menu:.search),
+    MenuItems(text:"Favorites",icon:RiveViewModel(fileName:"icons",artboardName:"LIKE/STAR"),menu:.favorites),
+    MenuItems(text:"Help",icon:RiveViewModel(fileName:"icons",artboardName:"CHAT"),menu:.help)
     ]
+var menuItems2:[MenuItems] = [
+    MenuItems(text:"History",icon:RiveViewModel(fileName:"icons",artboardName:"TIMER"),menu:.history),
+    MenuItems(text:"Notifications",icon:RiveViewModel(fileName:"icons",artboardName:"BELL"),menu:.notifications),
+    ]
+var menuItems3:[MenuItems] = [
+    MenuItems(text:"Dark Mode",icon:RiveViewModel(fileName:"icons",artboardName:"Settings"),menu:.darkMode),
+    ]
+enum SelectedMenu:String{
+    case home
+    case search
+    case favorites
+    case help
+    case history
+    case notifications
+    case darkMode
+}
